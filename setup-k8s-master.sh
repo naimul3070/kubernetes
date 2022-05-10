ef8ea566ef7cbd04d4f5d4ee1a56f2431afdc47e
Naimul islam <50922314+naimul3070@users.noreply.github.com>
2022-05-10 12:07:37 +0600

    Update setup-k8s-master.sh

--- setup-k8s-master.sh
+++ setup-k8s-master.sh
@@ -2,7 +2,7 @@
 
 # Initialize a kubernetes cluster on the current node.
 #
-KUBE_VERSION=1.18.2
+KUBE_VERSION=1.24.0
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version=$KUBE_VERSION
 
 mkdir -p $HOME/.kube
